d4d6577e61ba8f90f5d62a24f34bf72030925090
Tylor Clont <tclont@yahoo.com>
2022-12-25 01:29:54 -0500

    ARRAY | .Reverse() |

---
+++ codewars85.js
@@ -0,0 +1,9 @@
+function reverseList(list) {
+    return list.reverse()
+  }
+  // take in a list of what looks like array of integers / nums 
+  //return  array nums /int in reverse order
+  //flip the array of numbers
+  
+  
+  

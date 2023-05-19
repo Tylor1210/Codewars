2df7e5fdd438f407064563138a9598333458a0e8
Tylor C <100950093+Tylor1210@users.noreply.github.com>
2023-05-19 10:20:32 -0400

    arr | .indexOf | arr.find

---
+++ codewars120.js
@@ -0,0 +1,7 @@
+function findUniq(arr) {
+    return arr.find(
+      // The code below runs for every element of the array, it takes the element and checks if first position is the same as last position in the array
+      (item) => arr.indexOf(item) === arr.lastIndexOf(item) //
+    );
+  }
+  
